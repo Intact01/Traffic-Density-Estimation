e3dcf47cd18b015895b0c79a0125424c8bb9b6f8
Akshat <akku.gupta20801@gmail.com>
2021-02-24 03:02:31 +0530

    add comments readme added

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@
 using namespace std;
 
 int counter = 0;
-cv::Mat source_image;
-cv::Mat cache_image;
-cv::Mat original_image;
+cv::Mat source_image;       //input image with guide lines and mouse-click points
+cv::Mat cache_image;        //input image with mouse point tracking line
+cv::Mat original_image;     //input image
 cv::Scalar green = cv::Scalar(0, 255, 0);
+std::vector<cv::Point2f> source_points; //points clicked by user
 
-vector<cv::Point2f> source_points;
 
+//function declarations
 void mouse_callback(int event, int x, int y, int flag, void *param);
 void crop_image(cv::Mat, vector<cv::Point2f> points);
 
+//comparator functions used in sorting
 int cmp_x(const cv::Point2f &lhs, const cv::Point2f &rhs)
 {
     return lhs.x < rhs.x;
@@ -25,29 +27,37 @@ int cmp_y(const cv::Point2f &lhs, const cv::Point2f &rhs)
     return lhs.y < rhs.y;
 }
 
+
+//Tranformes perspective of original image
+//Stores transformed image
 void processImage()
 {
-    vector<cv::Point2f> destination_points;
+    vector<cv::Point2f> destination_points;                     //Points in the transformed image corresponding to the source_points 
     destination_points.push_back(cv::Point2f(472, 52));
     destination_points.push_back(cv::Point2f(472, 830));
     destination_points.push_back(cv::Point2f(800, 52));
     destination_points.push_back(cv::Point2f(800, 830));
 
+    //sort source_points
     stable_sort(source_points.begin(), source_points.end(), cmp_x);
     stable_sort(source_points.begin(), source_points.begin() + 2, cmp_y);
     stable_sort(source_points.begin() + 2, source_points.begin() + 4, cmp_y);
 
-    cv::Mat homography_matrix = cv::findHomography(source_points, destination_points);
+    //calculate perspective transformation matrix
+    cv::Mat homography_matrix = cv::findHomography(source_points, destination_points);  
 
-    cv::Mat output_image;
-    cv::Mat output_image_with_lines;
+    cv::Mat output_image;                    //Transformed image
+    cv::Mat output_image_with_lines;         //Transformed image with lines displaying crop-area
+
+    //Transform perpective of original_image to output_image and store output_image to device
     cv::warpPerspective(original_image, output_image, homography_matrix, source_image.size());
-    output_image.copyTo(output_image_with_lines);
+    cv::imwrite("transformed.jpg", output_image);
 
+
+    output_image.copyTo(output_image_with_lines);
     cv::destroyWindow("Source Image");
 
-    // Draw points and lines on output image
-
+    // Draw points and lines on output_image_with_lines
     int order[]{0, 1, 3, 2, 0};
     for (int i = 0; i < 4; i++)
     {
@@ -55,71 +65,89 @@ void processImage()
         cv::line(output_image_with_lines, destination_points[order[i]], destination_points[order[i + 1]], green, 1, 8, 0);
     }
 
+    // Display message
     string helpText = "Press any key to crop, Esc. to cancel";
     cv::Point textPos = cv::Point2f(destination_points[0].x - 100, (destination_points[0].y + destination_points[1].y) / 2);
     cv::putText(output_image_with_lines, helpText, textPos, cv::FONT_HERSHEY_PLAIN, 2.0, green, 2, true);
+
+    //show output_image_with_lines
     cv::imshow("Transformed Image", output_image_with_lines);
     cout << helpText << endl;
 
+    //wait for user to press a key
     int key = cv::waitKey(0);
+    
+    //if key is escape => cancel else crop the transformed image
     if (key == 27)
     {
         exit(-1);
     }
-
-    cv::imwrite("transformed.jpg", output_image);
     cv::destroyWindow("Transformed Image");
-
     crop_image(output_image, destination_points);
 }
 
+// crops given image img into a rectanglular area described by given points
+//stores cropped image
 void crop_image(cv::Mat img, vector<cv::Point2f> points)
 {
-
+    // crop area  
     cv::Rect crop_area;
-
     crop_area.width = points[3].x - points[0].x;
     crop_area.height = points[1].y - points[0].y;
     crop_area.x = points[0].x;
     crop_area.y = points[0].y;
 
+    // crop the image
+    // show and store the cropped image
     cv::Mat cropped_image = img(crop_area);
     cv::imshow("Cropped Image", cropped_image);
     cv::imwrite("cropped.jpg", cropped_image);
     cv::waitKey(0);
 }
 
+// tracks mouse events
 void mouse_callback(int event, int x, int y, int flag, void *param)
 {
     cv::Point2f newpt = cv::Point2f(x, y);
+
+    // left click 
     if (event == cv::EVENT_LBUTTONDOWN)
     {
-        source_points.push_back(newpt);
         counter++;
 
+        //till 4 points are selected
         if (counter <= 4)
         {
+            source_points.push_back(newpt);
+
+            //display circle at clicked point
             cv::circle(source_image, newpt, 5, green, 2, 8, 0);
             cv::imshow("Source Image", source_image);
         }
         if (counter > 1 && counter <= 4)
-        {
+        {   
+            // join clicked point with previously clicked point 
             cv::line(source_image, source_points[counter - 2], source_points[counter - 1], green, 1, 8, 0);
             cv::imshow("Source Image", source_image);
         }
 
         cout << x << " " << y << endl;
         if (counter == 4)
-        {
+        {   
+            // complete the rectangle
             cv::line(source_image, source_points[0], source_points[counter - 1], green, 1, 8, 0);
             cv::imshow("Source Image", source_image);
             processImage();
         }
     }
+
+    // mouse pointer tracking
     else if (event == cv::EVENT_MOUSEMOVE)
-    {
+    {   
+        //until 4 points are selected
         if (counter > 0 && counter < 4)
-        {
+        {   
+            // show mouse pointer by a line joined to previously clicked point
             source_image.copyTo(cache_image);
             cv::line(cache_image, source_points[counter - 1], newpt, green, 1, 8, 0);
             cv::imshow("Source Image", cache_image);
@@ -127,20 +155,28 @@ void mouse_callback(int event, int x, int y, int flag, void *param)
     }
 }
 
+
+// main function
 int main(int argc, char **argv)
 {    
-    string image_name;
+    string image_name;      //image path/name
 
-    image_name = parse(argc,argv);
+    image_name = parse(argc,argv);  
     if (image_name == "") return -1;
+    
     source_image = cv::imread(image_name, cv::IMREAD_GRAYSCALE);
+
+    //wrong image name
     if (source_image.empty())
     {
         cout << "Could not open or find the image : " << image_name << endl;
         return -1;
     }
+
     cache_image = source_image.clone();
     original_image = cache_image.clone();
+
+    //display input image
     cv::imshow("Source Image", source_image);
 
     cv::setMouseCallback("Source Image", mouse_callback);
