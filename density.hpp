3c90398c4e4d5916cb9185cc0681d78c451f67e2
saptarshi <2001saptarshi@gmail.com>
2021-03-07 14:31:57 +0530

    used dilation and thresholding

--- density.hpp
+++ density.hpp
@@ -31,22 +31,34 @@ vector<double> queue_density(cv::VideoCapture capture, int fast_forward)
         cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
         frame = cameraCorrection(frame, scr_pts, dest_pts);
 
+        int erosion_size = 1;
+        cv::Mat element1 = cv::getStructuringElement(cv::MORPH_RECT,
+                                                     cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+                                                     cv::Point(erosion_size, erosion_size));
+        int dilation_size = 3;
+        cv::Mat element2 = cv::getStructuringElement(cv::MORPH_RECT,
+                                                     cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+                                                     cv::Point(dilation_size, dilation_size));
         pBackSub->apply(frame, fgMask, 0);
 
-        fgMask.copyTo(frame1);
-        // cv::threshold(fgMask, frame1, 127, 255, cv::THRESH_BINARY);
-
-        // cv::erode(frame1,frame1,)
+        // fgMask.copyTo(frame1);
+        cv::Mat thresh;
+        cv::erode(fgMask, thresh, element1);
+        cv::threshold(thresh, thresh, 207, 255, cv::THRESH_BINARY);
+        //
+        cv::dilate(thresh, frame1, element2);
         // cv::normalize(fgMask, frame1, 0.0f, 1.0f, cv::NORM_MINMAX);
-        double sum = cv::sum(frame1)[0];
-        cout << sum << " " << counter << endl;
-        // int val = cv::countNonZero(frame1);
-        queue_density_list.push_back(sum);
+        // thresh.copyTo(frame1);
+        // double sum = cv::sum(frame1)[0];
+        // cout << sum << " " << counter << endl;
+        int val = cv::countNonZero(frame1);
+        queue_density_list.push_back(val);
         // cout << val << " " << counter << endl;
 
         imshow("Frame", frame);
         imshow("FG Mask", fgMask);
-        imshow("thresh", frame1);
+        // imshow("thresh", thresh);
+        imshow("frame1", frame1);
 
         int keyboard = cv::waitKey(30);
         if (keyboard == 'q' || keyboard == 27)
